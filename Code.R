af930b8febe4f5bababe9277ca6b20460009164c
Oleksiy Anokhin <oleksiy.anokhin@gmail.com>
2019-08-01 17:16:27 -0400

    Initial commit

---
+++ Code.R
@@ -0,0 +1,14 @@
+# Oleksiy Anokhin
+
+# August 1, 2019
+
+# Creating a website for D2D project
+
+# Install 
+library(blogdown)
+
+# Create a new template
+blogdown::build_site()
+
+# Run web-site
+blogdown::serve_site()
