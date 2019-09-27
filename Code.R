88291eedb8268cb2f1632a8e2798f553121dac80
Oleksiy Anokhin <oleksiy.anokhin@gmail.com>
2019-09-27 12:54:16 -0400

    Added a new version, removed an old version to a separate folder

--- Code.R
+++ Code.R
@@ -1,28 +1,8 @@
 # Oleksiy Anokhin
+# September 16, 2019
 
-# August 1, 2019
-
-# Creating a website for D2D project
-
-# Install 
 library(blogdown)
-
-# Create a new template
-blogdown::build_site()
-
-# Run web-site
 blogdown::serve_site()
 
 
-# To do list:
-
-# - Tabs: Tools, Photos, Projects, Countries, Partners, Links etc.
-# - Content: Pics, Flourish links, press releases, media links  etc
-# - Blog updates: three events, other projects (govt, work groups, ngos etc)
-# - Fix sizes of the Tools icons
-# - Clarify how to host pictures internally
-
-# How to organize content on Hugo?
-# https://gohugo.io/content-management/organization/
-  
-  
+# blogdown::new_post("New post test", ext = '.Rmd')
---
+++ Old version of the website/Code.R
@@ -0,0 +1,28 @@
+# Oleksiy Anokhin
+
+# August 1, 2019
+
+# Creating a website for D2D project
+
+# Install 
+library(blogdown)
+
+# Create a new template
+blogdown::build_site()
+
+# Run web-site
+blogdown::serve_site()
+
+
+# To do list:
+
+# - Tabs: Tools, Photos, Projects, Countries, Partners, Links etc.
+# - Content: Pics, Flourish links, press releases, media links  etc
+# - Blog updates: three events, other projects (govt, work groups, ngos etc)
+# - Fix sizes of the Tools icons
+# - Clarify how to host pictures internally
+
+# How to organize content on Hugo?
+# https://gohugo.io/content-management/organization/
+  
+  
